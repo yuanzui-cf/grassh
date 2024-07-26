69c62ea8a4b4366e51c30b697447b366496afd35
Leo Jia <grassblogstu@gmail.com>
2024-07-26 22:45:47 +0800

    feat: Menu switcher

--- lib/src/component/menu.dart
+++ lib/src/component/menu.dart
@@ -4,9 +4,12 @@ import 'package:grassh/src/models/menu.dart';
 
 class Menu extends StatefulWidget {
   final List<MenuModel> menus;
+  final int initialIndex;
+
   const Menu({
     super.key,
     required this.menus,
+    this.initialIndex = 0,
   });
 
   @override
@@ -15,9 +18,10 @@ class Menu extends StatefulWidget {
 
 class _MenuState extends State<Menu> {
   bool isHover = false;
+  int currentIndex = -1;
 
-  Color getColor(MenuModel state) {
-    if (state.on || state.defaultOn) {
+  Color getColor(MenuModel state, int index) {
+    if (currentIndex == index || state.isHover) {
       return GlobalConfig.theme.shade100.withOpacity(0.3);
     }
     return GlobalConfig.theme.shade50.withOpacity(0);
@@ -25,52 +29,72 @@ class _MenuState extends State<Menu> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.menus.isNotEmpty && currentIndex == -1) {
+      if (widget.initialIndex >= widget.menus.length) {
+        currentIndex = 0;
+      } else {
+        currentIndex = widget.initialIndex;
+      }
+    }
+
     return ListView.builder(
       shrinkWrap: true,
       itemCount: widget.menus.length,
       itemBuilder: (ctx, index) {
-        return StatefulBuilder(
-          builder: (
-            BuildContext context,
-            void Function(void Function()) setState,
-          ) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 5,
-              ),
-              child: MouseRegion(
-                cursor: SystemMouseCursors.click,
-                onEnter: (event) {
-                  setState(() {
-                    widget.menus[index].on = true;
-                  });
-                },
-                onExit: (event) {
-                  setState(() {
-                    widget.menus[index].on = false;
-                  });
-                },
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 200),
-                  curve: Curves.fastOutSlowIn,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: getColor(widget.menus[index]),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Padding(
+        List<Widget> opt = [];
+        if (widget.menus[index].icon != null) {
+          opt.addAll([
+            Icon(
+              widget.menus[index].icon,
+            ),
+            const SizedBox(width: 5),
+          ]);
+        }
+        opt.add(Text(
+          widget.menus[index].title,
+        ));
+        return Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 5,
+          ),
+          child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            onEnter: (event) {
+              setState(() {
+                widget.menus[index].isHover = true;
+              });
+            },
+            onExit: (event) {
+              setState(() {
+                widget.menus[index].isHover = false;
+              });
+            },
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  currentIndex = index;
+                });
+              },
+              borderRadius: BorderRadius.circular(5),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.fastOutSlowIn,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: getColor(widget.menus[index], index),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
                     padding: const EdgeInsets.symmetric(
                       vertical: 10,
                       horizontal: 10,
                     ),
-                    child: Text(
-                      widget.menus[index].title,
-                    ),
-                  ),
-                ),
+                    child: Row(
+                      children: opt,
+                    )),
               ),
-            );
-          },
+            ),
+          ),
         );
       },
     );
--- lib/src/models/menu.dart
+++ lib/src/models/menu.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 class MenuModel {
   final IconData? icon;
   final String title;
-  bool defaultOn;
-  bool on = false;
+  bool isHover = false;
 
   MenuModel({
     required this.title,
     this.icon,
-    this.defaultOn = false,
   });
 }
