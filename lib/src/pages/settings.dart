6902f96c8d919e54938cfe2fd9d1c1422c9315d3
Leo Jia <grassblogstu@gmail.com>
2024-07-27 13:39:38 +0800

    feat: Setting page tab bar

--- lib/src/pages/settings.dart
+++ lib/src/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grassh/src/component/automatic_keep_alive_wrapper.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -7,11 +8,48 @@ class SettingsPage extends StatefulWidget {
   State<SettingsPage> createState() => _SettingsPageState();
 }
 
-class _SettingsPageState extends State<SettingsPage> {
+class _SettingsPageState extends State<SettingsPage>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+  List tabs = ["Global", "Backup", "About"];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text("Settings"),
+    return Column(
+      children: [
+        Expanded(
+          flex: 0,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: TabBar(
+              controller: _tabController,
+              tabs: tabs.map((e) => Tab(text: e)).toList(),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: TabBarView(
+              controller: _tabController,
+              children: tabs.map((e) {
+                return KeepAliveWrapper(
+                  child: Container(
+                    alignment: Alignment.center,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
