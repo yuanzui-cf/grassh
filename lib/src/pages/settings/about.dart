5b89da036150503c36219b5c4eb3901b6a9fc66a
yuanzui_cf <garssblogstu@gmail.com>
2024-07-28 19:05:59 +0800

    feat: About page

--- lib/src/pages/settings/about.dart
+++ lib/src/pages/settings/about.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:grassh/src/config/global_config.dart';
 
 class AboutSettings extends StatefulWidget {
   const AboutSettings({super.key});
@@ -10,6 +12,36 @@ class AboutSettings extends StatefulWidget {
 class _AboutSettingsState extends State<AboutSettings> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        const SizedBox(
+          width: 100,
+          height: 100,
+          child: Image(
+            image: AssetImage("assets/images/Logo.webp"),
+          ),
+        ),
+        Text(
+          AppLocalizations.of(context)!.title,
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: GlobalConfig.theme.withGreen(50).withBlue(10).withRed(10),
+          ),
+        ),
+        Text(
+          "v${GlobalConfig.packageInfo.version}",
+        ),
+        const Text(
+          "Copyright (c) 2024 Grass Development Team.",
+          style: TextStyle(
+            fontSize: 12,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+      ],
+    );
   }
 }
