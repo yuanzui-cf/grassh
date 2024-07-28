7f184458b81f29778b58f86f42239d903b10b320
yuanzui_cf <garssblogstu@gmail.com>
2024-07-28 21:57:42 +0800

    feat: Add config read & write

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -3,33 +3,54 @@ import 'dart:io';
 import 'package:toml/toml.dart';
 
 class Config {
-  Map<String, dynamic>? conf;
+  late Map<String, dynamic> conf;
   String path;
 
   Config({
     required this.path,
     required Map<String, dynamic> defaultConfig,
   }) {
+    conf = defaultConfig;
     init(path, defaultConfig);
   }
 
   init(String path, Map<String, dynamic> defaultConfig) {
     var f = File(path);
+    _configFileCheck(f);
+    read();
+  }
+
+  save() {
+    var f = File(path);
+    _configFileCheck(f);
+    try {
+      var config = TomlDocument.fromMap(conf).toString();
+      f.writeAsStringSync(config);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  read() {
+    var f = File(path);
+    _configFileCheck(f);
+    var ctx = f.readAsStringSync();
+    try {
+      conf = TomlDocument.parse(ctx).toMap();
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  _configFileCheck(File f) {
     if (!f.existsSync()) {
       f.createSync();
       try {
-        var config = TomlDocument.fromMap(defaultConfig).toString();
+        var config = TomlDocument.fromMap(conf).toString();
         f.writeAsStringSync(config);
       } catch (e) {
         rethrow;
       }
-    }
-    var ctx = f.readAsStringSync();
-    try {
-      var config = TomlDocument.parse(ctx).toMap();
-      conf = config;
-    } catch (e) {
-      rethrow;
     }
   }
 }
